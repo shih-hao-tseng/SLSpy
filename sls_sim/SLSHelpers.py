93e0d6f9290a2ec0c77fb8bc8e2a78dfd76c65dc
Shih-Hao Tseng <shtseng@caltech.edu>
2019-10-31 14:00:10 -0700

    add

--- sls_sim/SLSHelpers.py
+++ sls_sim/SLSHelpers.py
@@ -8,7 +8,7 @@ def SLS_Objective_Value_H2 (C1, D12, Phi_x=[], Phi_u=[]):
     '''
     objective_value = 0
     for tau in range(len(Phi_x)):
-        objective_value += cp.sum_squares(C1  * Phi_x[tau] + D12 * Phi_u[tau])
+        objective_value += cp.sum_squares(C1*Phi_x[tau] + D12*Phi_u[tau])
     
     return objective_value
 
