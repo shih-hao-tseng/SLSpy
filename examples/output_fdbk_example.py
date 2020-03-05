4d055c28cbdace6a07ee1d0899d4340145ff6197
Shih-Hao Tseng <shtseng@caltech.edu>
2020-03-05 13:46:23 -0800

    update the output feedback example

--- examples/output_fdbk_example.py
+++ examples/output_fdbk_example.py
@@ -35,26 +35,37 @@ def output_fdbk_example():
         FIR_horizon = 20,
         state_feedback = False # use output-feedback synthesizer
     )
+    # the default objective SLS_Obj_LQ() is equivalent to SLS_Obj_H2
+    synthesizer << SLS_Obj_LQ()
 
-    # assume uncorrelated measurement noise
-    mm_noise_amp = 1.0
-    cov_v_sqrt  = mm_noise_amp * np.eye(10)
-    
-    # set SLS objective    
-    synthesizer << SLS_Obj_LQ(Cov_v_sqrt=cov_v_sqrt)
-   
     # synthesize controller (the generated controller is actually initialized)
     # and use the synthesized controller in simulation
-    controller = synthesizer.synthesizeControllerModel ()
     simulator.setController (
-        controller = controller
+        controller = synthesizer.synthesizeControllerModel ()
     )
 
     # run the simulation
     x_history, _, _, u_history, _ = simulator.run ()
 
     Bu_history = matrix_list_multiplication(sys._B2,u_history)
-    plot_heat_map(x_history, Bu_history, 'Centralized')
+    plot_heat_map(x_history, Bu_history, 'Default')
+
+    # we can also test the case with some correlation structure
+    # assume uncorrelated measurement noise
+    mm_noise_amp = 1.0
+    cov_v_sqrt  = mm_noise_amp * np.eye(sys._Ny)
+  
+    synthesizer << SLS_Obj_LQ(Cov_v_sqrt=cov_v_sqrt)
+    # try the correlated version
+    simulator.setController (
+        controller = synthesizer.synthesizeControllerModel ()
+    )
+
+    # run the simulation
+    x_history, _, _, u_history, _ = simulator.run ()
+
+    Bu_history = matrix_list_multiplication(sys._B2,u_history)
+    plot_heat_map(x_history, Bu_history, 'Correlated')
 
 if __name__ == '__main__':
     output_fdbk_example()
